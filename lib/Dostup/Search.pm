99a8c3d83a2bad63dcb137fbf22a9a5cb8f3e9ab
vrag86 <vrag@localhost.localdomain>
2013-09-17 22:58:50 +0400

    dbi connect

--- lib/Dostup/Search.pm
+++ lib/Dostup/Search.pm
@@ -1,7 +1,23 @@
 package Dostup::Search;
 use Mojo::Base 'Mojolicious::Controller';
 use DBI;
+our $dbh;
+&db_connect;
+
+
 #Подключаемся к БД
+
+sub db_connect
+{
+  $dbh=DBI->connect("DBI:mysql:database=Check;host=$db_host;port=3306", 'vrag', 'a3797953A') or die "\nCan't connect to mysql";
+#Включаем UTF8  
+  $dbh->{'mysql_enable_utf8'} = 1;
+  $dbh->do('set names utf8');
+  return 1;
+}
+
+
+
 
 
 

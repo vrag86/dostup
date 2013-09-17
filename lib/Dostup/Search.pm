19500c7b9ddb9c29241da8088e3358d75e625c98
vrag86 <vrag@localhost.localdomain>
2013-09-18 01:46:49 +0400

    new featers

--- lib/Dostup/Search.pm
+++ lib/Dostup/Search.pm
@@ -2,6 +2,7 @@ package Dostup::Search;
 use Mojo::Base 'Mojolicious::Controller';
 use DBI;
 our $dbh;
+our $db_host='85.21.12.22';
 &db_connect;
 
 
@@ -9,7 +10,7 @@ our $dbh;
 
 sub db_connect
 {
-  $dbh=DBI->connect("DBI:mysql:database=Check;host=$db_host;port=3306", 'vrag', 'a3797953A') or die "\nCan't connect to mysql";
+  $dbh=DBI->connect("DBI:mysql:database=dostup;host=$db_host;port=3306", 'vrag', 'a3797953A') or die "\nCan't connect to mysql";
 #Включаем UTF8  
   $dbh->{'mysql_enable_utf8'} = 1;
   $dbh->do('set names utf8');
@@ -27,12 +28,29 @@ sub search
   my $address=$self->param('address');
   if (defined($address))
   {
-    $self->render(message=>$address);  
+    my @tp=&search_tp($address);
+    $self->render(tp=>\@tp);  
   }
   else
   {
    $self->render(message=>'');
   }
+}
+
+
+
+sub search_tp
+{
+ my $address=shift;
+    $address='%'.$address.'%';
+ my @tp;
+ my $query="select site_name from info where upper(Address) like upper('$address')";   #Изменить на site_code
+ my $sth=$dbh->prepare($query); $sth->execute();
+ while (my $u=($sth->fetchrow_array))
+ {
+  push @tp, $u;
+ }
+ return @tp;
 }
 
 
